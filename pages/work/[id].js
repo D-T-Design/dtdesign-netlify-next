ab24688255fc0fd06025c61283699eca763463a4
David Torres <info@davidtorres.design>
2021-02-01 02:51:12 -0800

    Reverse Unoptomize feature

--- pages/work/[id].js
+++ pages/work/[id].js
@@ -62,7 +62,6 @@ export default function Project({ projectData }) {
 								width={954}
                 height={592}
                 quality={100}
-                unoptimized
                 className="project-photo"
                 key={index}
 							/>
