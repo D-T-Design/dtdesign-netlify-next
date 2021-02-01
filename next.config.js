b83e0d28c7fcef711fa3f40314bf1f167082bc08
David Torres <info@davidtorres.design>
2021-02-01 03:01:30 -0800

    Try Netlify public img host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-	images: {
-		loader: "cloudinary",
-		path: "https://res.cloudinary.com/dtdesign/",
-	},
-};
+// module.exports = {
+// 	images: {
+// 		loader: "cloudinary",
+// 		path: "https://res.cloudinary.com/dtdesign/",
+// 	},
+// };
