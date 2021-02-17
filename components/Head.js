2e35a2719e4beda575656957a8499dee99d03d66
David Torres <info@davidtorres.design>
2021-02-16 22:20:09 -0800

    Added more og tags to Head

--- components/Head.js
+++ components/Head.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function HeadCustom(props) {
+	const currentURL = useRouter().asPath;
 	return (
 		<Head>
 			<title>{props.title}</title>
 			<meta name="description" content={props.description} />
 			<meta property="og:title" content={props.title} key="title" />
 			<meta property="og:description" content={props.description} />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={`https://davidtorres.design${currentURL}`} />
 		</Head>
 	);
 }
