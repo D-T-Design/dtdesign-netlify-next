29da01525f39f86f767ac4c71bd28b1e89f8312a
David Torres <info@davidtorres.design>
2021-02-16 22:59:05 -0800

    Added loading animation between pages

--- pages/_app.js
+++ pages/_app.js
@@ -7,16 +7,26 @@ import * as gtag from "../lib/gtag";
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
 import client from "../client";
+import NProgress from "nprogress";
+
+NProgress.configure({ showSpinner: false });
 
 const App = ({ Component, pageProps, download }) => {
 	const router = useRouter();
+	const startLoading = () => NProgress.start();
+	const doneLoading = () => NProgress.done();
 	useEffect(() => {
 		const handleRouteChange = (url) => {
+			NProgress.done();
 			gtag.pageview(url);
 		};
+		router.events.on("routeChangeStart", startLoading);
 		router.events.on("routeChangeComplete", handleRouteChange);
+		router.events.on("routeChangeError", doneLoading);
 		return () => {
+			router.events.off("routeChangeStart", doneLoading);
 			router.events.off("routeChangeComplete", handleRouteChange);
+			router.events.off("routeChangeError", doneLoading);
 		};
 	}, [router.events]);
 
