8c496c50b52b0e210882028bdcd2c0e4cdbac707
David Torres <info@davidtorres.design>
2021-02-25 19:01:22 -0800

    Convert Contact Form to useState Hook instead of Class component

--- components/Form.js
+++ components/Form.js
@@ -1,6 +1,6 @@
-import React from "react";
-// import { useRouter } from "next/router";
-import { withRouter } from "next/router";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+// import { withRouter } from "next/router";
 
 function encode(data) {
 	return Object.keys(data)
@@ -8,135 +8,162 @@ function encode(data) {
 		.join("&");
 }
 
-class ContactForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { name: "", email: "", message: "" };
-	}
-	handleSubmit = (e) => {
-		console.log(this.state, this.props);
-		fetch("/", {
-			method: "POST",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: encode({ "form-name": "contact", ...this.state }),
-		})
-			.then(() => this.props.router.push("/"))
-			.catch((error) => alert(error));
-
-		e.preventDefault();
-	};
-	handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
-	render() {
-		const { name, email, message } = this.state;
-		return (
-			<form
-				name="contact"
-				method="POST"
-				// action="/"
-				netlify-honeypot="bot-field"
-				data-netlify="true"
-				onSubmit={this.handleSubmit}
-			>
-				<input type="hidden" name="form-name" value="contact" />
-
-				<p className="name">
-					<label htmlFor="yourname">Your Name:</label> <br />
-					<input type="text" name="name" value={name} id="yourname" onChange={this.handleChange} />
-				</p>
-
-				<p className="email">
-					<label htmlFor="youremail">Your Email:</label> <br />
-					<input
-						type="email"
-						name="email"
-						value={email}
-						id="youremail"
-						onChange={this.handleChange}
-					/>
-				</p>
-
-				<p className="message">
-					<label htmlFor="yourmessage">Message:</label> <br />
-					<textarea
-						name="message"
-						value={message}
-						id="yourmessage"
-						onChange={this.handleChange}
-					></textarea>
-				</p>
-
-				<p className="send">
-					<button type="submit">Send Message</button>
-				</p>
-
-				<p className="hidden">
-					<label>
-						Don’t fill this out if you’re human: <input name="bot-field" />
-					</label>
-				</p>
-			</form>
-		);
-	}
-}
-
-export default withRouter(ContactForm);
-// export default function ContactForm() {
-// 	function encode(data) {
-// 		return Object.keys(data)
-// 			.map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-// 			.join("&");
+// class ContactForm extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = { name: "", email: "", message: "" };
 // 	}
 
-// 	const handleSubmit = (event) => {
-// 		event.preventDefault();
-// 		console.log(event);
+// 	handleSubmit = (e) => {
+// 		console.log(this.state, this.props);
 // 		fetch("/", {
 // 			method: "POST",
 // 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-// 			body: encode({
-// 				"form-name": event.target.getAttribute("name"),
-// 				...name,
-// 			}),
+// 			body: encode({ "form-name": "contact", ...this.state }),
 // 		})
-// 			.then(() => router.push("/"))
+// 			.then(() => this.props.router.push("/"))
 // 			.catch((error) => alert(error));
+
+// 		e.preventDefault();
 // 	};
 
-// 	return (
-// 		<form
-// 			name="contact"
-// 			method="POST"
-// 			// action="/"
-// 			netlify-honeypot="bot-field"
-// 			data-netlify="true"
-// 			onSubmit={handleSubmit}
-// 		>
-// 			<input type="hidden" name="form-name" value="contact" />
+// 	handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-// 			<p className="name">
-// 				<label htmlFor="yourname">Your Name:</label> <br />
-// 				<input type="text" name="name" id="yourname" />
-// 			</p>
+// 	render() {
+// 		const { name, email, message } = this.state;
+// 		return (
+// 			<form
+// 				name="contact"
+// 				method="POST"
+// 				// action="/"
+// 				netlify-honeypot="bot-field"
+// 				data-netlify="true"
+// 				onSubmit={this.handleSubmit}
+// 			>
+// 				<input type="hidden" name="form-name" value="contact" />
 
-// 			<p className="email">
-// 				<label htmlFor="youremail">Your Email:</label> <br />
-// 				<input type="email" name="email" id="youremail" />
-// 			</p>
+// 				<p className="name">
+// 					<label htmlFor="yourname">Your Name:</label> <br />
+// 					<input type="text" name="name" value={name} id="yourname" onChange={this.handleChange} />
+// 				</p>
 
-// 			<p className="message">
-// 				<label htmlFor="yourmessage">Message:</label> <br />
-// 				<textarea name="message" id="yourmessage"></textarea>
-// 			</p>
+// 				<p className="email">
+// 					<label htmlFor="youremail">Your Email:</label> <br />
+// 					<input
+// 						type="email"
+// 						name="email"
+// 						value={email}
+// 						id="youremail"
+// 						onChange={this.handleChange}
+// 					/>
+// 				</p>
 
-// 			<p className="send">
-// 				<button type="submit">Send Message</button>
-// 			</p>
+// 				<p className="message">
+// 					<label htmlFor="yourmessage">Message:</label> <br />
+// 					<textarea
+// 						name="message"
+// 						value={message}
+// 						id="yourmessage"
+// 						onChange={this.handleChange}
+// 					></textarea>
+// 				</p>
 
-// 			<p className="hidden">
-// 				<label>
-// 					Don’t fill this out if you’re human: <input name="bot-field" />
-// 				</label>
-// 			</p>
-// 		</form>
-// 	);
+// 				<p className="send">
+// 					<button type="submit">Send Message</button>
+// 				</p>
+
+// 				<p className="hidden">
+// 					<label>
+// 						Don’t fill this out if you’re human: <input name="bot-field" />
+// 					</label>
+// 				</p>
+// 			</form>
+// 		);
+// 	}
 // }
+
+// export default withRouter(ContactForm);
+
+export default function SendMessageForm() {
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const updateFormData = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const router = useRouter();
+
+	const handleSubmit = (e) => {
+		console.log(formData);
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({ "form-name": "contact", ...formData }),
+		})
+			.then(() => router.push("/"))
+			.catch((error) => alert(error));
+		e.preventDefault();
+	};
+
+	return (
+		<form
+			name="contact"
+			method="POST"
+			// action="/"
+			netlify-honeypot="bot-field"
+			data-netlify="true"
+			onSubmit={handleSubmit}
+		>
+			<input type="hidden" name="form-name" value="contact" />
+
+			<p className="name">
+				<label htmlFor="yourname">Your Name:</label> <br />
+				<input
+					type="text"
+					name="name"
+					value={formData.name}
+					id="yourname"
+					onChange={updateFormData}
+				/>
+			</p>
+
+			<p className="email">
+				<label htmlFor="youremail">Your Email:</label> <br />
+				<input
+					type="email"
+					name="email"
+					value={formData.email}
+					id="youremail"
+					onChange={updateFormData}
+				/>
+			</p>
+
+			<p className="message">
+				<label htmlFor="yourmessage">Message:</label> <br />
+				<textarea
+					name="message"
+					value={formData.message}
+					id="yourmessage"
+					onChange={updateFormData}
+				></textarea>
+			</p>
+
+			<p className="send">
+				<button type="submit">Send Message</button>
+			</p>
+
+			<p className="hidden">
+				<label>
+					Don’t fill this out if you’re human: <input name="bot-field" />
+				</label>
+			</p>
+		</form>
+	);
+}
